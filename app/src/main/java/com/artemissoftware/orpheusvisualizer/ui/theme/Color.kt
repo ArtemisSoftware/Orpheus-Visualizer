8379d5e682e9cec054aa79405938c62b23a939f2
gustavo <gustavo.isaac@gmail.com>
2023-08-17 05:37:43 +0100

    PlayPauseButton

--- app/src/main/java/com/artemissoftware/orpheusvisualizer/ui/theme/Color.kt
+++ app/src/main/java/com/artemissoftware/orpheusvisualizer/ui/theme/Color.kt
@@ -1,5 +1,6 @@
 package com.artemissoftware.orpheusvisualizer.ui.theme
 
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -10,7 +11,15 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+val Gray300 = Color(0xFFe0e0e0)
 val Gray400 = Color(0xFFbdbdbd)
 val onSurface = Color(0xFF637394)
 
 val Pink500 = Color(0xFFe91e63)
+
+val ButtonGradient = Brush.verticalGradient(
+    listOf(
+        Color(0xFF39AFEA),
+        Color(0xFF16DAD8),
+    ),
+)
