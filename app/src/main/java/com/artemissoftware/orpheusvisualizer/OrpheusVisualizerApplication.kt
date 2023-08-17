b242e0362b99cdf35562cc76ca77e353fdc65be8
gustavo <gustavo.isaac@gmail.com>
2023-08-17 06:43:25 +0100

    OrpheusVisualizerApplication

---
+++ app/src/main/java/com/artemissoftware/orpheusvisualizer/OrpheusVisualizerApplication.kt
@@ -0,0 +1,7 @@
+package com.artemissoftware.orpheusvisualizer
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class OrpheusVisualizerApplication : Application()
